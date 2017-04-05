2987d2bf0250448b085da13c1ac18e4d4d4a8657
Oleg Voronin <voronin@andewil.com>
2017-04-05 03:34:56 +0300

    HW servlet

---
+++ src/main/java/FdControl.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by andev on 05.04.2017.
+ */
+@javax.servlet.annotation.WebServlet(name = "FdControl")
+public class FdControl extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        // response.sendError(901, "asdasddas");
+        PrintWriter out = response.getWriter();
+        out.print("<h1>Hello!</h1>");
+    }
+}
---
+++ src/main/java/FdStream.java
@@ -0,0 +1,6 @@
+/**
+ * Created by andev on 05.04.2017.
+ */
+public class FdStream {
+
+}
